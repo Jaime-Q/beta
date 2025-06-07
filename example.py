e09581affe1f347789680c023fce627216e0a5d1
Jaime <jaimequenan@gmail.com>
2025-06-06 21:32:24 -0500

    Script: data types in Python

---
+++ example.py
@@ -0,0 +1,23 @@
+#This is an example with Pyton
+'''
+Date: 06-jun-2025
+Dev: Jaime Quenan
+'''
+data1 = 10
+data2 = 3,50
+data3 = "Jaime"
+data4 = 'Quenan'
+data5 = '''
+Hello people, we are learning Python.
+That's wonderful
+'''
+data6 = True
+data7 = 3.4
+
+print(type(data1))
+print(type(data2))
+print(type(data3))
+print(type(data4))
+print(type(data5))
+print(type(data6))
+print(type(data7))
