ed3be67f56f1c027a3ed80ed2c27861071429401
Jaime <jaimequenan@gmail.com>
2025-06-29 19:31:00 -0500

    Winning Dice Game

---
+++ example6.py
@@ -0,0 +1,21 @@
+import os
+from random import randint, uniform
+
+def ged_dices():
+    os.system('clear')
+    print("::: WELCOM TO MY PARCHIS :::")
+    
+    player_name = input("Player name: ")
+
+    dice1 = randint(1,6)
+    dice2 = randint(1,6)
+
+    print(f"Dice 1: {dice1}")
+    print(f"Dice 2: {dice2}")
+
+    if dice1 % 2 == 0 and dice2 % 2 == 0:
+        print(":: 🎉 YOU WIND 🎉 ::")
+    else:
+        print(":: 😢 YOU LOSE 😢 ::")
+
+ged_dices()
