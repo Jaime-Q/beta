6f4cd2738106bfb69a85db47f317a78c82c76e1b
Johana <moncayojohana27@gmail.com>
2025-07-10 20:58:53 -0500

    example6

--- example6.py
+++ example6.py
@@ -13,8 +13,8 @@ def ged_dices():
     print(f"Dice 2: {dice2}")
 
     if dice1 % 2 == 0 and dice2 % 2 == 0:
-        print(":: 🎉 YOU WIND 🎉 ::")
+        print("::  YOU WIND  ::")
     else:
-        print(":: 😢 YOU LOSE 😢 ::")
+        print(":: YOU LOSE ::")
 
 ged_dices()
